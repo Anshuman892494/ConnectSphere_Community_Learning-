7a3a1b8acd19ac85687a3fff5802766b2af9e393
Anshuman Varma <anshumanverma9795@gmail.com>
2026-06-30 20:07:21 +0530

    Part 4: Frontend Localization Context & Translation Dictionaries

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import { ToastProvider } from './contexts/ToastContext';
+import { LanguageProvider } from './contexts/LanguageContext';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <Provider store={store}>
       <ToastProvider>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
+        <LanguageProvider>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </LanguageProvider>
       </ToastProvider>
     </Provider>
   );
